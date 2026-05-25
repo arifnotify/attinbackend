3fecfe598330167b812c6932f673e241c835fda6
Arif <90087547+arifnotify@users.noreply.github.com>
2026-05-25 10:03:40 +0300

    Refactor AuthController for improved readability

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 
@@ -8,21 +12,30 @@ import { VerifyOtpDto } from './dto/verify-otp.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+  ) {}
 
+  // SEND OTP
   @Post('send-otp')
   sendOtp(
     @Body()
     sendOtpDto: SendOtpDto,
   ) {
-    return this.authService.sendOtp(sendOtpDto.phone);
+    return this.authService.sendOtp(
+      sendOtpDto.phone,
+    );
   }
 
+  // VERIFY OTP
   @Post('verify-otp')
   verifyOtp(
     @Body()
     verifyOtpDto: VerifyOtpDto,
   ) {
-    return this.authService.verifyOtp(verifyOtpDto.phone, verifyOtpDto.otp);
+    return this.authService.verifyOtp(
+      verifyOtpDto.phone,
+      verifyOtpDto.otp,
+    );
   }
 }
