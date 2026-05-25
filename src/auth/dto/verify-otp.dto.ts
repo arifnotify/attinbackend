a518a9a483180799b0514377b834c5e8ab75b58e
Arif <90087547+arifnotify@users.noreply.github.com>
2026-05-25 10:01:45 +0300

    Format imports and adjust OTP validation rules

--- src/auth/dto/verify-otp.dto.ts
+++ src/auth/dto/verify-otp.dto.ts
@@ -1,11 +1,16 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import {
+  IsNotEmpty,
+  Length,
+} from 'class-validator';
 
 export class VerifyOtpDto {
   @IsNotEmpty()
+
   @Length(11, 11)
   phone: string;
 
   @IsNotEmpty()
+
   @Length(4, 4)
   otp: string;
 }
