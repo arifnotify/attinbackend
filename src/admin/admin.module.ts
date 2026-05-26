11cfc4dd6095482843f0f865a1e3edc9d422b00d
arifnotify <arifnotify@gmail.com>
2026-05-26 04:19:00 +0300

    update 8

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,26 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 
 import { Admin, AdminSchema } from './schemas/admin.schema';
 
-import { AuthModule } from '../auth/auth.module'; // ⭐ ADD THIS
-
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      {
-        name: Admin.name,
-        schema: AdminSchema,
-      },
-    ]),
+    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
 
-    AuthModule, // ⭐ THIS FIXES JwtService ERROR
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
   ],
 
   controllers: [AdminController],
   providers: [AdminService],
 })
 export class AdminModule {}
+
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
-    PassportModule,
+    UsersModule,
 
     JwtModule.register({
       secret: process.env.JWT_SECRET,
@@ -14,8 +15,7 @@ import { AuthService } from './auth.service';
     }),
   ],
 
+  controllers: [AuthController],
   providers: [AuthService],
-
-  exports: [JwtModule], // ⭐ VERY IMPORTANT
 })
 export class AuthModule {}
