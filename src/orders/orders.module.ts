eb24266b29678e1b3e9ab0cbf45f5eab64500735
Arif <90087547+arifnotify@users.noreply.github.com>
2026-05-31 18:50:07 +0300

    Update orders.module.ts

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,50 +1,22 @@
 import { Module } from '@nestjs/common';
-
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { OrdersController } from './orders.controller';
-
 import { OrdersService } from './orders.service';
 
-import {
-  Order,
-  OrderSchema,
-} from './schemas/order.schema';
-
-import {
-  Cart,
-  CartSchema,
-} from '../cart/schemas/cart.schema';
-
-import {
-  User,
-  UserSchema,
-} from '../users/schemas/user.schema';
-
-import { RedisModule } from '../redis/redis.module';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { Cart, CartSchema } from '../cart/schemas/cart.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      {
-        name: Order.name,
-        schema: OrderSchema,
-      },
-      {
-        name: Cart.name,
-        schema: CartSchema,
-      },
-      {
-        name: User.name,
-        schema: UserSchema,
-      },
+      { name: Order.name, schema: OrderSchema },
+      { name: Cart.name, schema: CartSchema },
+      { name: User.name, schema: UserSchema },
     ]),
-
-    RedisModule,
   ],
-
   controllers: [OrdersController],
-
   providers: [OrdersService],
 })
 export class OrdersModule {}
