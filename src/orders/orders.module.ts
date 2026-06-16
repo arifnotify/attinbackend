f6c2ccdd104151bd72527fcf75276f9aee377e7f
Arif <90087547+arifnotify@users.noreply.github.com>
2026-06-16 17:23:06 +0300

    Refactor imports and schema registration in OrdersModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,25 +4,59 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
-import { Order, OrderSchema } from './schemas/order.schema';
-import { Cart, CartSchema } from '../cart/schemas/cart.schema';
-import { User, UserSchema } from '../users/schemas/user.schema';
+import {
+  Order,
+  OrderSchema,
+} from './schemas/order.schema';
+
+import {
+  RiderLocation,
+  RiderLocationSchema,
+} from './rider-location/rider-location.schema';
+
+import {
+  Cart,
+  CartSchema,
+} from '../cart/schemas/cart.schema';
+
+import {
+  User,
+  UserSchema,
+} from '../users/schemas/user.schema';
 
 import { RedisModule } from '../redis/redis.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Order.name, schema: OrderSchema },
-      { name: Cart.name, schema: CartSchema },
-      { name: User.name, schema: UserSchema },
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+
+      {
+        name: RiderLocation.name,
+        schema: RiderLocationSchema,
+      },
+
+      {
+        name: Cart.name,
+        schema: CartSchema,
+      },
+
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
     ]),
 
     RedisModule,
   ],
 
   controllers: [OrdersController],
+
   providers: [OrdersService],
+
   exports: [OrdersService],
 })
 export class OrdersModule {}
