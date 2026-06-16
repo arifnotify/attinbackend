641a3493f3351458ca4a5d95a0a389fbae903b88
Arif <90087547+arifnotify@users.noreply.github.com>
2026-06-16 17:22:02 +0300

    Update orders.service.ts

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+
 import { Model } from 'mongoose';
 
 import { Order, OrderDocument } from './schemas/order.schema';
 import { User } from '../users/schemas/user.schema';
 import { Cart } from '../cart/schemas/cart.schema';
 
+import {
+  RiderLocation,
+  RiderLocationDocument,
+} from './rider-location/rider-location.schema';
+
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+
 import { OrderStatus } from './enums/order-status.enum';
 
 @Injectable()
@@ -21,21 +28,36 @@ export class OrdersService {
 
     @InjectModel(Cart.name)
     private cartModel: Model<any>,
+
+    @InjectModel(RiderLocation.name)
+    private riderLocationModel: Model<RiderLocationDocument>,
   ) {}
 
   // =========================
   // CREATE ORDER
   // =========================
-  async createOrder(userId: string, dto: CreateOrderDto) {
-    const user = await this.userModel.findById(userId);
-    if (!user) throw new NotFoundException('User not found');
+
+  async createOrder(
+    userId: string,
+    dto: CreateOrderDto,
+  ) {
+    const user =
+      await this.userModel.findById(userId);
+
+    if (!user) {
+      throw new NotFoundException(
+        'User not found',
+      );
+    }
 
     const cartItems = await this.cartModel
       .find({ user: userId })
       .populate('product');
 
     if (!cartItems.length) {
-      throw new NotFoundException('Cart is empty');
+      throw new NotFoundException(
+        'Cart is empty',
+      );
     }
 
     const totalAmount = cartItems.reduce(
@@ -46,124 +68,177 @@ export class OrdersService {
     const items = cartItems.map((item) => ({
       product: item.product._id,
       productName: item.product.title,
-      productImage: item.product.images?.[0] || '',
+      productImage:
+        item.product.images?.[0] || '',
       quantity: item.quantity,
       price: item.price,
       totalPrice: item.totalPrice,
     }));
 
-    // =========================
-    // 8 DIGIT ORDER NUMBER
-    // =========================
     let orderNumber = '';
     let exists = true;
 
     while (exists) {
-      orderNumber = Math.floor(10000000 + Math.random() * 90000000).toString();
+      orderNumber = Math.floor(
+        10000000 + Math.random() * 90000000,
+      ).toString();
 
-      const check = await this.orderModel.findOne({ orderNumber });
+      const check =
+        await this.orderModel.findOne({
+          orderNumber,
+        });
 
-      if (!check) exists = false;
+      if (!check) {
+        exists = false;
+      }
     }
 
-    const order = await this.orderModel.create({
-      orderNumber,
-      user: userId,
-      customerPhone: user.phone,
-      shippingAddress: dto.shippingAddress,
-      items,
-      totalAmount,
-      paymentMethod: 'COD',
-      orderStatus: OrderStatus.PENDING,
-      isPaid: false,
-      trackingEnabled: false,
-    });
+    const order =
+      await this.orderModel.create({
+        orderNumber,
+        user: userId,
+        customerPhone: user.phone,
+        shippingAddress:
+          dto.shippingAddress,
+        items,
+        totalAmount,
+        paymentMethod: 'COD',
+        orderStatus:
+          OrderStatus.PENDING,
+        isPaid: false,
+        trackingEnabled: false,
+      });
 
-    await this.cartModel.deleteMany({ user: userId });
+    await this.cartModel.deleteMany({
+      user: userId,
+    });
 
     return order;
   }
 
   // =========================
-  // MY ORDERS
+  // USER ORDERS
   // =========================
+
   async getUserOrders(userId: string) {
-    return this.orderModel.find({ user: userId }).sort({ createdAt: -1 });
+    return this.orderModel
+      .find({ user: userId })
+      .sort({ createdAt: -1 });
   }
 
   // =========================
-  // ALL ORDERS (ADMIN)
+  // ALL ORDERS
   // =========================
+
   async getAllOrders() {
-    return this.orderModel.find().sort({ createdAt: -1 });
+    return this.orderModel
+      .find()
+      .sort({ createdAt: -1 });
   }
 
   // =========================
   // SINGLE ORDER
   // =========================
+
   async getSingleOrder(id: string) {
-    const order = await this.orderModel.findById(id);
-    if (!order) throw new NotFoundException('Order not found');
+    const order =
+      await this.orderModel.findById(id);
+
+    if (!order) {
+      throw new NotFoundException(
+        'Order not found',
+      );
+    }
+
     return order;
   }
 
   // =========================
   // UPDATE STATUS
   // =========================
-  async updateOrderStatus(id: string, dto: UpdateOrderStatusDto) {
+
+  async updateOrderStatus(
+    id: string,
+    dto: UpdateOrderStatusDto,
+  ) {
     return this.orderModel.findByIdAndUpdate(
       id,
-      { orderStatus: dto.orderStatus },
-      { new: true },
+      {
+        orderStatus: dto.orderStatus,
+      },
+      {
+        new: true,
+      },
     );
   }
 
   // =========================
   // ASSIGN RIDER
   // =========================
-  async assignRider(orderId: string, riderId: string) {
+
+  async assignRider(
+    orderId: string,
+    riderId: string,
+  ) {
     return this.orderModel.findByIdAndUpdate(
       orderId,
       {
         assignedRider: riderId,
-        orderStatus: OrderStatus.OUT_FOR_DELIVERY,
+        orderStatus:
+          OrderStatus.OUT_FOR_DELIVERY,
         trackingEnabled: true,
-      },
-      { new: true },
-    );
-  }
-
-  // =========================
-  // TRACK ORDER
-  // =========================
-  async getTracking(orderId: string) {
-    const order = await this.orderModel.findById(orderId);
-
-    if (!order) throw new NotFoundException('Order not found');
-
-    return {
-      orderNumber: order.orderNumber,
-      status: order.orderStatus,
-      trackingEnabled: order.trackingEnabled,
-      assignedRider: order.assignedRider,
-
-      riderLat: order.riderLat,
-      riderLng: order.riderLng,
-      lastLocationUpdate: order.lastLocationUpdate,
-};
-  }
-  //////////////////
-  async updateRiderLocation(orderId: string, lat: number, lng: number) {
-    return this.orderModel.findByIdAndUpdate(
-      orderId,
-      {
-        riderLat: lat,
-        riderLng: lng,
-        lastLocationUpdate: new Date(),
       },
       {
         new: true,
       },
     );
+  }
+
+  // =========================
+  // TRACKING
+  // =========================
+
+  async getTracking(orderId: string) {
+    const order =
+      await this.orderModel.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundException(
+        'Order not found',
+      );
+    }
+
+    if (!order.assignedRider) {
+      return {
+        orderNumber: order.orderNumber,
+        status: order.orderStatus,
+        trackingEnabled: false,
+      };
+    }
+
+    const riderLocation =
+      await this.riderLocationModel.findOne({
+        riderId: order.assignedRider,
+      });
+
+    return {
+      orderNumber: order.orderNumber,
+      status: order.orderStatus,
+      trackingEnabled:
+        order.trackingEnabled,
+
+      assignedRider:
+        order.assignedRider,
+
+      riderLat:
+        riderLocation?.lat ?? null,
+
+      riderLng:
+        riderLocation?.lng ?? null,
+
+      lastLocationUpdate:
+        riderLocation?.updatedAt ??
+        null,
+    };
   }
 }
