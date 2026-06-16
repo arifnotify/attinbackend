2114da09c5d601c7f19354af7db3bd9193c4ce5f
Arif <90087547+arifnotify@users.noreply.github.com>
2026-06-16 17:16:22 +0300

    Update rider-location.schema.ts

--- src/orders/rider-location/rider-location.schema.ts
+++ src/orders/rider-location/rider-location.schema.ts
@@ -1,21 +1,36 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
-export type RiderLocationDocument = RiderLocation & Document;
+export type RiderLocationDocument =
+  RiderLocation & Document;
 
 @Schema({ timestamps: true })
 export class RiderLocation {
-  @Prop({ type: Types.ObjectId, ref: 'Rider', required: true })
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Rider',
+    required: true,
+    unique: true,
+  })
   riderId: Types.ObjectId;
 
-  @Prop({ type: Number, required: true })
+  @Prop({
+    type: Number,
+    required: true,
+  })
   lat: number;
 
-  @Prop({ type: Number, required: true })
+  @Prop({
+    type: Number,
+    required: true,
+  })
   lng: number;
 
-  @Prop({ default: Date.now })
+  @Prop({
+    default: Date.now,
+  })
   updatedAt: Date;
 }
 
-export const RiderLocationSchema = SchemaFactory.createForClass(RiderLocation);
+export const RiderLocationSchema =
+  SchemaFactory.createForClass(RiderLocation);
