b39146541a5ee2ec25c430749c4a0e036251323d
arifnotify <arifnotify@gmail.com>
2026-06-18 11:52:16 +0300

    first commit 5

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
-
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 import { Order, OrderDocument } from './schemas/order.schema';
 import { User } from '../users/schemas/user.schema';
