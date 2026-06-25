dce53259c8c4c13b819fd0fee6fb88327db77750
arifnotify <arifnotify@gmail.com>
2026-06-25 22:07:24 +0300

    first commit 4

--- src/support-links/dto/update-support-link.dto.ts
+++ src/support-links/dto/update-support-link.dto.ts
@@ -1,7 +1,23 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class UpdateSupportLinkDto {
+  @IsOptional()
+  @IsString()
   whatsapp?: string;
+
+  @IsOptional()
+  @IsString()
   phone?: string;
+
+  @IsOptional()
+  @IsString()
   facebook?: string;
+
+  @IsOptional()
+  @IsString()
   instagram?: string;
+
+  @IsOptional()
+  @IsString()
   messenger?: string;
 }
