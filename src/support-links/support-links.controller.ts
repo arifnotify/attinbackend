5992e9086afee852b4122e95eb6f7e522ed6d613
arifnotify <arifnotify@gmail.com>
2026-06-26 10:04:53 +0300

    first commit 2

--- src/support-links/support-links.controller.ts
+++ src/support-links/support-links.controller.ts
@@ -1,16 +1,16 @@
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
 
 import { SupportLinksService } from './support-links.service';
 import { UpdateSupportLinkDto } from './dto/update-support-link.dto';
 
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+//import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('support-links')
 export class SupportLinksController {
   constructor(private readonly service: SupportLinksService) {}
 
   // UPDATE
-  @UseGuards(JwtAuthGuard)
+  //@UseGuards(JwtAuthGuard)
   @Patch()
   update(@Body() dto: UpdateSupportLinkDto) {
     return this.service.updateLinks(dto);
