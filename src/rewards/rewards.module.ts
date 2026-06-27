dfa2523d82ee9bf647eb52531627ec8135029f1f
arifnotify <arifnotify@gmail.com>
2026-06-27 15:01:04 +0300

    first commit 4

--- src/rewards/rewards.module.ts
+++ src/rewards/rewards.module.ts
@@ -1,42 +1,28 @@
 import { Module } from '@nestjs/common';
-
 import { MongooseModule } from '@nestjs/mongoose';
 
-import {
-  RewardWallet,
-  RewardWalletSchema,
-} from './schemas/reward-wallet.schema';
-
-import {
-  RewardTransaction,
-  RewardTransactionSchema,
-} from './schemas/reward-transaction.schema';
-
-import { RewardsController } from './rewards.controller';
 import { RewardsService } from './rewards.service';
+import { RewardsController } from './rewards.controller';
 
-import { RewardSettingsModule } from '../reward-settings/reward-settings.module';
+import { RewardWallet, RewardWalletSchema } from './schemas/reward-wallet.schema';
+import { RewardTransaction, RewardTransactionSchema } from './schemas/reward-transaction.schema';
+
+import { RewardSettingsModule } from 'src/reward-settings/reward-settings.module';
+import { CouponsModule } from 'src/coupons/coupons.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      {
-        name: RewardWallet.name,
-        schema: RewardWalletSchema,
-      },
-      {
-        name: RewardTransaction.name,
-        schema: RewardTransactionSchema,
-      },
+      { name: RewardWallet.name, schema: RewardWalletSchema },
+      { name: RewardTransaction.name, schema: RewardTransactionSchema },
     ]),
 
     RewardSettingsModule,
+    CouponsModule, // 🔥 IMPORTANT FIX
   ],
 
-  controllers: [RewardsController],
-
   providers: [RewardsService],
-
+  controllers: [RewardsController],
   exports: [RewardsService],
 })
 export class RewardsModule {}
