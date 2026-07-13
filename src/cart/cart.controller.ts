0a2100ff219b481f22fd9ebe396dbae5cba187c7
arifnotify <arifnotify@gmail.com>
2026-07-13 13:59:13 +0300

    first commit 45

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -65,4 +65,15 @@ export class CartController {
   removeCartItem(@Param('id') id: string) {
     return this.cartService.removeCartItem(id);
   }
+
+  //////////////////////////////////////////////
+  @Get('refresh')
+@UseGuards(JwtAuthGuard)
+  refreshCart(@Req() req: any) {
+
+return this.cartService.refreshCart(
+ req.user.userId
+);
+
+}
 }
