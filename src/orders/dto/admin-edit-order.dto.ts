58a96df8b4c3a44ee51a606610ea52ee61b58c9d
Arif <90087547+arifnotify@users.noreply.github.com>
2026-07-23 14:19:02 +0300

    Enhance validation in AdminEditOrderDto and ItemDto
    Added validation for product name and image fields in ItemDto. Updated MultiLanguageTextDto and refined AdminEditOrderDto structure.

--- src/orders/dto/admin-edit-order.dto.ts
+++ src/orders/dto/admin-edit-order.dto.ts
@@ -4,41 +4,54 @@ import {
   IsNumber,
   IsOptional,
   ValidateNested,
+  IsObject,
+  IsNotEmpty,
 } from 'class-validator';
-
 import { Type } from 'class-transformer';
 
+// Language Title DTO
+class MultiLanguageTextDto {
+  @IsString()
+  @IsNotEmpty()
+  en: string;
 
-class ItemDto {
-
-
-@IsString()
-  product: string;
-
-
-@IsNumber()
-quantity:number;
-
-
-@IsNumber()
-price:number;
-
-
-@IsOptional()
-@IsString()
-productName?:string;
-
-
-@IsOptional()
-@IsString()
-productImage?:string;
+  @IsString()
+  @IsNotEmpty()
+  bn: string;
 }
 
+// Item DTO
+class ItemDto {
+  @IsString()
+  product: string;
 
+  @IsNumber()
+  quantity: number;
+
+  @IsNumber()
+  price: number;
+
+  // en & bn validation
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => MultiLanguageTextDto)
+  productName?: MultiLanguageTextDto;
+
+  // unit validation
+  @IsOptional()
+  @IsString()
+  unit?: string;
+
+  @IsOptional()
+  @IsString()
+  productImage?: string;
+}
+
+// Main DTO
 export class AdminEditOrderDto {
-@IsArray()
-@ValidateNested({each:true})
-@Type(()=>ItemDto)
-items:ItemDto[];
-
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ItemDto)
+  items: ItemDto[];
 }
