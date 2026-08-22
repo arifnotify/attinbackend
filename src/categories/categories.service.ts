3597a26f9ec201a0c386ef91b869c5d11d134774
Arif <90087547+arifnotify@users.noreply.github.com>
2026-08-22 17:26:43 +0300

    Import Types from mongoose in categories.service.ts

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types  } from 'mongoose';
 
 import { Category, CategoryDocument } from './schemas/category.schema';
 
