b9af6ad90654d3a0270fe41e21d643a1725f81dc
arifnotify <arifnotify@gmail.com>
2026-08-28 10:53:29 +0300

    first commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,6 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { RedisService } from '../redis/redis.service';
-import { SmsService } from 'src/sms/sms.service';
 
 @Injectable()
 export class AuthService {
@@ -14,71 +13,36 @@ export class AuthService {
     private readonly usersService: UsersService,
     private readonly redisService: RedisService,
     private readonly jwtService: JwtService,
-    private readonly smsService: SmsService,
   ) {}
 
   // =========================
   // SEND OTP
   // =========================
-async sendOtp(phone: string) {
-  // CHECK USER BLOCKED
-  const user = await this.usersService.findByPhone(phone);
+  async sendOtp(phone: string) {
+    // CHECK USER BLOCKED
+    const user = await this.usersService.findByPhone(phone);
 
-  if (user?.isBlocked) {
-    throw new UnauthorizedException(
-      `This number is blocked. Reason: ${user.blockReason}`,
-    );
+    if (user?.isBlocked) {
+      throw new UnauthorizedException(
+        `This number is blocked. Reason: ${user.blockReason}`,
+      );
+    }
+
+    // TEST OTP
+    const otp = '1234';
+
+    // DEBUG LOG
+    console.log('Generated OTP:', otp);
+
+    // SAVE OTP IN REDIS (5 min)
+    await this.redisService.set(`otp:${phone}`, otp, 300);
+
+    return {
+      success: true,
+      message: 'OTP sent successfully',
+      otp,
+    };
   }
-
-  // OTP COOLDOWN (1 minute)
-  const cooldown = await this.redisService.get(
-    `otp-cooldown:${phone}`,
-  );
-
-  if (cooldown) {
-    throw new UnauthorizedException(
-      'Please wait 1 minute before requesting another OTP',
-    );
-  }
-
-  // GENERATE RANDOM OTP
-  const otp = Math.floor(
-    1000 + Math.random() * 9000,
-  ).toString();
-
-  // FORMAT PHONE NUMBER
-  const formattedPhone = phone.startsWith('0')
-    ? `88${phone}`
-    : phone;
-
-  // DEBUG LOG
-  console.log('Generated OTP:', otp);
-
-  // SEND SMS
-  await this.smsService.sendOtp(
-    formattedPhone,
-    otp,
-  );
-
-  // SAVE OTP IN REDIS (5 min)
-  await this.redisService.set(
-    `otp:${phone}`,
-    otp,
-    300,
-  );
-
-  // OTP REQUEST COOLDOWN (1 min)
-  await this.redisService.set(
-    `otp-cooldown:${phone}`,
-    '1',
-    60,
-  );
-
-  return {
-    success: true,
-    message: 'OTP sent successfully',
-  };
-}
 
   // =========================
   // VERIFY OTP
